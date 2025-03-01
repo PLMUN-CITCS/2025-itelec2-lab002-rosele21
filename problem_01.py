6bf9337a602c86a8d767342a4cc32ebdb03755ab
rosele21 <>
2025-03-01 15:06:50 +0800

    Last Sync: 2025-03-01 07:06:50 (Mobile)

--- problem_01.py
+++ problem_01.py
@@ -1,4 +1,4 @@
-# YOUR NAME
+# Rosele Joy Paraguya 
 # ITELEC2
 # Problem Set 01 - Problem 01
 # Simple Calculator Program
--- problem_02.py
+++ problem_02.py
@@ -1,4 +1,4 @@
-# YOUR NAME
+# Rosele Joy Paraguya 
 # ITELEC2
 # Problem Set 01 - Problem 02
 # Square the Number Program
